daf1bf891fa9e8eb4c9ad005a252f240baf7332c
RhoSeungA <sskclara@naver.com>
2024-01-15 17:33:07 +0900

    init

---
+++ src/main/java/com/example/BapZip/BapZipApplication.java
@@ -0,0 +1,13 @@
+package com.example.BapZip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BapZipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BapZipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BapZip/BapZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BapZip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BapZipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
