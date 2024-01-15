d02689fbf195dc9aabb2add1e67f213ec76cb9dc
RhoSeungA <sskclara@naver.com>
2024-01-15 18:16:38 +0900

    Store,User 엔티티 생성

---
+++ src/main/java/com/example/BapZip/domain/Store.java
@@ -0,0 +1,23 @@
+package com.example.BapZip.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@DynamicUpdate
+@DynamicInsert
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+}
