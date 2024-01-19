e774349aa1fef241284650c505a9ddba780d9aab
hyeonjichoi21 <ch5i_hj15@naver.com>
2024-01-19 17:23:01 +0900

    healthController  테스트

---
+++ src/main/java/com/example/BapZip/web/controller/healthController.java
@@ -0,0 +1,15 @@
+package com.example.BapZip.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class healthController {
+    @GetMapping("/health")
+    public String checkHealth ()
+    {
+        return "I'm healthy!!";
+    }
+
+}
