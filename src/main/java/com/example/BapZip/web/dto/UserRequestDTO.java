061395f50a64a323db028779edd713e414cdae1a
RhoSeungA <sskclara@naver.com>
2024-01-22 18:45:27 +0900

    #31 feat: 회원가입 requestDTO 생성

--- src/main/java/com/example/BapZip/web/dto/UserRequestDTO.java
+++ src/main/java/com/example/BapZip/web/dto/UserRequestDTO.java
@@ -1,4 +1,33 @@
 package com.example.BapZip.web.dto;
 
+import com.example.BapZip.domain.School;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRequestDTO {
+    @Builder
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class JoinDTO{
+        private String userId;
+
+        private String password;
+
+        private String nickname;
+
+        private School school;
+
+        private String major;
+
+        private String email;
+
+    }
 }
