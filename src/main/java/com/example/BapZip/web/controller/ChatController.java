50e6fffe92a8e06feb50c7a7a09d52f641806170
RhoSeungA <sskclara@naver.com>
2024-01-26 12:16:15 +0900

    #52 chore: 컨트롤러 초반 세팅

---
+++ src/main/java/com/example/BapZip/web/controller/ChatController.java
@@ -0,0 +1,11 @@
+package com.example.BapZip.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+public class ChatController {
+}
