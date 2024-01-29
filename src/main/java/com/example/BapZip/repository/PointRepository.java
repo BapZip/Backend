fb4a073f93d40080b155eb672288e1808257ef65
Jsh <king601143@gmail.com>
2024-01-29 13:21:43 +0900

    get point History API

--- src/main/java/com/example/BapZip/repository/PointRepository.java
+++ src/main/java/com/example/BapZip/repository/PointRepository.java
@@ -1,10 +1,14 @@
 package com.example.BapZip.repository;
 
 import com.example.BapZip.domain.Point;
+import com.example.BapZip.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 
 public interface PointRepository extends JpaRepository<Point, Long> {
+    Optional<User> findByUserId(String userId);
 }
--- src/main/java/com/example/BapZip/web/dto/PointResponseDTO.java
+++ src/main/java/com/example/BapZip/web/dto/PointResponseDTO.java
@@ -1,4 +1,20 @@
 package com.example.BapZip.web.dto;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
 public class PointResponseDTO {
+
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    public static class PointHistoryDTO{
+        private Long pointId;
+        private Long point;
+        private String classification;
+        private String note;
+    }
 }
