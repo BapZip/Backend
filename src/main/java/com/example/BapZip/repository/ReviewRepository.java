648fcba3aff0fdf6be072e2b8a112ec28d7a9a50
hyeonjichoi21 <ch5i_hj15@naver.com>
2024-02-11 20:43:48 +0900

    #122 feat : 리뷰 조회 최신순 정렬

--- src/main/java/com/example/BapZip/repository/ReviewRepository.java
+++ src/main/java/com/example/BapZip/repository/ReviewRepository.java
@@ -22,8 +22,8 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findAllByStore(Store store);
 
 
-    // 특정 사용자의 모든 리뷰를 검색하기 위한 쿼리 메서드 (리스트로 리뷰 목록 쉽게 O)
-    List<Review> findAllByUserId(Long userId);
+    // 특정 사용자의 모든 리뷰를 검색하기 위한 쿼리 메서드 (리스트로 리뷰 목록 쉽게 O) (최신순 정렬)
+    List<Review> findAllByUserIdOrderByCreatedAtDesc(Long userId);
 
     List<Review> findByImagesImageUrl(String imageUrl);
 
